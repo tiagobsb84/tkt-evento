b6d8d33340f7c0fe96e3d1e5e045b92463084cd4
Tiago Silva <tiagobsb84@gmail.com>
2024-04-22 04:20:39 -0300

    Adicionando a classe controller da aplicacao

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EventController::class, 'index']);
 
 Route::get('/contact', function() {
     return view('contact');
